7cea42ac88bb3cfbb72536947fda1eebcad03fd4
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2021-04-30 11:56:55 -0500

    Translate shaderpack option values in the config gui

--- src/main/java/net/coderbot/iris/gui/property/IntOptionProperty.java
+++ src/main/java/net/coderbot/iris/gui/property/IntOptionProperty.java
@@ -3,6 +3,7 @@ package net.coderbot.iris.gui.property;
 import net.coderbot.iris.gui.GuiUtil;
 import net.coderbot.iris.gui.element.PropertyDocumentWidget;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -20,7 +21,9 @@ public class IntOptionProperty extends OptionProperty<Integer> {
 
 	@Override
     public Text createValueText(int width) {
-        return GuiUtil.trimmed(MinecraftClient.getInstance().textRenderer, Integer.toString(this.getValue()), width, false, true, isDefault() ? Formatting.RESET : Formatting.YELLOW);
+    	String translation = "value." + key + "." + this.getValue();
+    	boolean hasTranslation = I18n.hasTranslation(translation);
+        return GuiUtil.trimmed(MinecraftClient.getInstance().textRenderer, hasTranslation ? translation : Integer.toString(this.getValue()), width, hasTranslation, true, isDefault() ? Formatting.RESET : Formatting.YELLOW);
     }
 
     @Override
