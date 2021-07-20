76bca4f6869f890470dc295698f3120884265f03
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2021-07-20 16:28:28 -0500

    Revert "Add a config option to disable the buffered entity rendering optimizations in Iris (may workaround memory leak issues at the expense of some frames when rendering lots of entities)"
    The leak has been found and it wasn't the entity rendering optimizations, this serves no purpose anymore
    This reverts commit ce27bc432bddb6fb910c4204855d174ab260bc07.

--- src/main/java/net/coderbot/iris/mixin/fantastic/MixinWorldRenderer.java
+++ src/main/java/net/coderbot/iris/mixin/fantastic/MixinWorldRenderer.java
@@ -1,6 +1,5 @@
 package net.coderbot.iris.mixin.fantastic;
 
-import net.coderbot.iris.Iris;
 import net.coderbot.iris.fantastic.ExtendedBufferStorage;
 import net.coderbot.iris.fantastic.FlushableVertexConsumerProvider;
 import net.coderbot.iris.fantastic.ParticleRenderingPhase;
@@ -59,9 +58,7 @@ public class MixinWorldRenderer {
 
 	@Inject(method = "render", at = @At("HEAD"))
 	private void iris$fantastic$beginWorldRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo callback) {
-		if (!Iris.getIrisConfig().shouldDisableEntityRenderingOptimizations()) {
-			((ExtendedBufferStorage) bufferBuilders).beginWorldRendering();
-		}
+		((ExtendedBufferStorage) bufferBuilders).beginWorldRendering();
 	}
 
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/render/RenderLayer.getTranslucent ()Lnet/minecraft/client/render/RenderLayer;"))
@@ -77,8 +74,6 @@ public class MixinWorldRenderer {
 
 	@Inject(method = "render", at = @At("RETURN"))
 	private void iris$fantastic$endWorldRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo callback) {
-		if (!Iris.getIrisConfig().shouldDisableEntityRenderingOptimizations()) {
-			((ExtendedBufferStorage) bufferBuilders).endWorldRendering();
-		}
+		((ExtendedBufferStorage) bufferBuilders).endWorldRendering();
 	}
 }
